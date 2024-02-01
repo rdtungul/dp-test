82765d810d67471ee1fbe50b665fe3d320bc9c98
rdtungul <rosephdarl@gmail.com>
2024-02-01 21:23:51 +0800

    full design (no hover effects)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
